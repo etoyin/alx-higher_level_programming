07886053f1aae8860868fe46c847a6ff8ba76e28
etoyin <toyinadesina60@gmail.com>
2022-11-30 14:16:09 -0800

    task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 tracker = 1
 for c in range(ord('z'), ord('a')+1, -1):
     if tracker % 2 == 0:
-        i = chr(ord(c) - 32)
+        i = chr(c - 32)
         print('{}'.format(i), end='')
     else:
         print('{}'.format(chr(c)), end='')
