c5a6eaae0ad683c46e9cc987125ac6e61df54596
etoyin <toyinadesina60@gmail.com>
2022-11-30 10:13:13 -0800

    Task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(101):
+        if i % 15 == 0:
+            print("fizzbuzz", end="")
+        elif i % 3 == 0:
+            print("fizz", end="")
+        elif i % 5 == 0:
+            print("buzz", end="")
+        else:
+            print(i, end="")
+    print("")
